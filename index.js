28f90fb21590dd02db2a12f2f3a18760b05159ed
serref jaguraga <reef163@gmail.com>
2022-01-13 21:30:47 -0500

    Assignment done

---
+++ index.js
@@ -0,0 +1,11 @@
+function introduction(name='Serref'){
+return `Hi, my name is ${name}.`
+}
+
+function introductionWithLanguage(name='Serref', language='Java'){
+return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+function introductionWithLanguageOptional(name='User', language='JavaScript'){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+    }
